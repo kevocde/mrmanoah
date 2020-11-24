8e6f510b51103743355918d28f786162ecf78500
kevocde <kevocde@gmail.com>
2020-11-23 21:49:18 -0500

    Commit inicial, proyecto viejo

---
+++ contenido/beneficios.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/beneficios.css">
+</head>
+<body>
+    <section id="info">
+        <h3></h3>
+        <div class="contenedor">
+            <div class="info-imagen">
+                <img src="../images/banner/instalacion.jpg">
+                <h4>INSTALACION</h4>
+            </div>
+            <div class="info-imagen">
+                <img src="../images/banner/mantenimiento.jpg" >
+                <h4>MANTENIMIENTO</h4>
+            </div>
+            <div class="info-imagen">
+                <img src="../images/banner/reparacion.jpg">
+                <h4>REPARACION</h4>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
---
+++ contenido/nuestra_historia.php
@@ -0,0 +1,2 @@
+<p>Contamos con un personal altamente calificado en el mantenimiento, la reparacion y la instalacion de tus electrodomesticos, confia en nosotros y haremos que para ti todo sea lo mas satisfactorio, comodo y sencillo posible, comunicate ahora mismo!</p>
+
